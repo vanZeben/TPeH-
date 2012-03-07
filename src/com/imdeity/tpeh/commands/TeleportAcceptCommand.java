441acb57b4c106852fda5b98d66c1ed8b6f584ba
Ryan vanZeben <vanZeben@live.ca>
2012-03-07 05:18:30 -0500

    update to potentially correct some things with teleport warmups

--- src/com/imdeity/tpeh/commands/TeleportAcceptCommand.java
+++ src/com/imdeity/tpeh/commands/TeleportAcceptCommand.java
@@ -24,15 +24,13 @@ public class TeleportAcceptCommand implements CommandExecutor {
 	}
 
 	@Override
-	public boolean onCommand(CommandSender sender, Command command,
-			String commandLabel, String[] args) {
+	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
 		if (sender instanceof Player) {
 			Player player = (Player) sender;
 			if (Deity.perm.has(player, "deity.teleport.accept")) {
 				return wrapCommand(player, args);
 			} else {
-				Deity.chat.sendPlayerError(player, "Teleport",
-						"Insufficient Vespene Gas.");
+				Deity.chat.sendPlayerError(player, "Teleport", "Insufficient Vespene Gas.");
 			}
 		}
 		return false;
@@ -42,71 +40,52 @@ public class TeleportAcceptCommand implements CommandExecutor {
 		if (split.length == 1) {
 			Player teleportee = Deity.server.getOnlinePlayer(split[0]);
 			if (teleportee != null && teleportee.isOnline()) {
-				if (Deity.perm.isAdmin(teleportee)
-						|| Deity.perm.isSubAdmin(teleportee)) {
-					Deity.chat.sendPlayerError(player, "Teleport",
-							"You cannot teleport to admins.");
+				if (Deity.perm.isAdmin(teleportee) || Deity.perm.isSubAdmin(teleportee)) {
+					Deity.chat.sendPlayerError(player, "Teleport", "You cannot teleport to admins.");
 					return false;
 				}
 				if (teleportee.getWorld().getName().equalsIgnoreCase("events")) {
-					Deity.chat.sendPlayerError(player, "Teleport",
-							"You cannot teleport to the event world");
+					Deity.chat.sendPlayerError(player, "Teleport", "You cannot teleport to the event world");
 					return false;
 				}
 				if (player.getWorld().getName().equalsIgnoreCase("events")) {
-					Deity.chat.sendPlayerError(player, "Teleport",
-							"You cannot teleport while in the event world");
+					Deity.chat.sendPlayerError(player, "Teleport", "You cannot teleport while in the event world");
 					return false;
 				}
-				Deity.chat.sendPlayerMessage(teleportee, player.getName()
-						+ " has requested to teleport to you.");
+				Deity.chat.sendPlayerMessage(teleportee, player.getName() + " has requested to teleport to you.");
 				this.executeTeleport(player, teleportee);
 				return true;
 			}
 		} else
-			Deity.chat.sendPlayerMessage(player, "Teleport",
-					"Sorry but you there was an error in that command.");
+			Deity.chat.sendPlayerMessage(player, "Teleport", "Sorry but you there was an error in that command.");
 		return false;
 	}
 
 	public void executeTeleport(Player teleporter, Player teleportee) {
 
 		List<Option> options = new ArrayList<Option>();
-		options.add(new Option("agree", new ConfirmQuestionTask(teleporter,
-				teleportee) {
+		options.add(new Option("agree", new ConfirmQuestionTask(teleporter, teleportee) {
 			@Override
 			public void run() {
-				Deity.chat.sendPlayerMessage(teleporter, "Teleport",
-						"You teleported to " + teleportee.getName() + ".");
-				Deity.chat.sendPlayerMessage(teleportee, "Teleport",
-						teleporter.getName() + " teleported to you.");
-
-				Deity.player.teleportAsCommand(teleporter, teleportee.getLocation());
-				String sql = "INSERT INTO "
-						+ Deity.data.getDB().tableName("deity_", "teleports")
-						+ " (`type`, `teleporter`, `teleportee`, `is_allowed`) VALUES (?, ?, ?, ?);";
-				Deity.data.getDB().Write(sql, "tpa", teleporter.getName(),
-						teleportee.getName(), 1);
+				if (Deity.player.teleportAsCommand(teleporter, teleportee.getLocation())) {
+					Deity.chat.sendPlayerMessage(teleporter, "Teleport", "You teleported to " + teleportee.getName() + ".");
+					Deity.chat.sendPlayerMessage(teleportee, "Teleport", teleporter.getName() + " teleported to you.");
+					String sql = "INSERT INTO " + Deity.data.getDB().tableName("deity_", "teleports") + " (`type`, `teleporter`, `teleportee`, `is_allowed`) VALUES (?, ?, ?, ?);";
+					Deity.data.getDB().Write(sql, "tpa", teleporter.getName(), teleportee.getName(), 1);
+				}
 			}
 		}));
 
-		options.add(new Option("deny", new ConfirmQuestionTask(teleporter,
-				teleportee) {
+		options.add(new Option("deny", new ConfirmQuestionTask(teleporter, teleportee) {
 			@Override
 			public void run() {
-				Deity.chat.sendPlayerError(teleporter, "Teleport",
-						"Sorry the teleport was denied");
-				Deity.chat.sendPlayerMessage(teleportee, "Teleport",
-						"You denied the teleport");
-				String sql = "INSERT INTO "
-						+ Deity.data.getDB().tableName("deity_", "teleports")
-						+ " (`type`, `teleporter`, `teleportee`, `is_allowed`) VALUES (?, ?, ?, ?);";
-				Deity.data.getDB().Write(sql, "tpa", teleporter.getName(),
-						teleportee.getName(), 0);
+				Deity.chat.sendPlayerError(teleporter, "Teleport", "Sorry the teleport was denied");
+				Deity.chat.sendPlayerMessage(teleportee, "Teleport", "You denied the teleport");
+				String sql = "INSERT INTO " + Deity.data.getDB().tableName("deity_", "teleports") + " (`type`, `teleporter`, `teleportee`, `is_allowed`) VALUES (?, ?, ?, ?);";
+				Deity.data.getDB().Write(sql, "tpa", teleporter.getName(), teleportee.getName(), 0);
 			}
 		}));
-		Question question = new Question(teleportee.getName(),
-				"Do you agree to this teleport?", options);
+		Question question = new Question(teleportee.getName(), "Do you agree to this teleport?", options);
 		try {
 			plugin.appendQuestion(plugin.getQuestioner(), question);
 		} catch (Exception e) {
